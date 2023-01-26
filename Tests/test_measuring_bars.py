bba21228efa6a14653d6a2b1f7dcfaa73f86347a
Mark Gotham <mrhgotham@gmail.com>
2023-01-26 10:43:00 +0000

    Separate unittests

---
+++ Tests/test_measuring_bars.py
@@ -0,0 +1,12 @@
+"""
+Test the measure map processing.
+"""
+
+
+from unittest import TestCase
+
+
+class Test(TestCase):
+
+    def test_measure_map(self):
+        self.fail()
---
+++ Tests/test_music21_application.py
@@ -0,0 +1,11 @@
+"""
+Test the application to music21.
+"""
+
+from unittest import TestCase
+
+
+class Test(TestCase):
+
+    def test_example_case(self):
+        self.fail()
